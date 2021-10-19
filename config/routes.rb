9946b04c7f0454fa81c4298b29e04abd18d77a83
ymasa1213 <ymasa1213@yahoo.co.jp>
2021-10-19 06:35:10 +0000

    devise追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #会員
+  devise_for :customers,skip: [:password,], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+  }
+  #管理者
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+  }
 end
