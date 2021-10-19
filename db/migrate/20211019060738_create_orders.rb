6311210db93ecdc01e8b387c6309e6b681c3055e
elmo0509 <elmo.boof@gmail.com>
2021-10-19 06:47:12 +0000

    install-model

---
+++ db/migrate/20211019060738_create_orders.rb
@@ -0,0 +1,17 @@
+class CreateOrders < ActiveRecord::Migration[5.0]
+  def change
+    create_table :orders do |t|
+      
+      t.integer :customer_id, null: false
+      t.string :postcode, null: false
+      t.string :address, null: false
+      t.string :name, null: false
+      t.integer :carriage, null: false
+      t.integer :total_payment, null: false
+      t.integer :pay_method, null: false, default: 0
+      t.integer :status, null: false, default: 0
+
+      t.timestamps
+    end
+  end
+end
