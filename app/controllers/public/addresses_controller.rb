6e88b74721ceb7fcdf3e0d24d6d85fdb1f22ffb6
sayakanoa <reiluv75418@gmail.com>
2021-10-20 09:33:18 +0000

    index~update追加

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,2 +1,33 @@
 class Public::AddressesController < ApplicationController
+    
+    def index
+      @address = Address.new
+      @addresses = Address.all
+    end
+    
+    def create
+      address = Address.new(address_params)
+      address.save
+      redirect_to addresses_path
+    end
+
+    def edit
+      @address = Address.find(params[:id])
+    end
+    
+    def update
+      address = Address.find(params[:id])
+      address.update(address_params)
+      
+      
+    end
+    
+    def destroy
+    end
+    
+    private
+    def address_params
+    params.require(:address).permit(:postcode, :address, :name)
+    end
+    
 end
