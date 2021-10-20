ffebc5b09fceef4e5c87140fee015bbeb6e4e738
yamauchimiku <yamamochi7171@gmail.com>
2021-10-20 06:10:23 +0000

    途中

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,29 @@
 class Admin::ItemsController < ApplicationController
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admins_items_path
+  end
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:price, :introduction, :name, :image)
+  end
 end
