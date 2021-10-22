2eb41ef671cd1ad7ab95a68748aa1e01a4ba56df
sayakanoa <reiluv75418@gmail.com>
2021-10-22 14:22:11 +0000

    カートのindex機能

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,20 @@
 class Public::CartItemsController < ApplicationController
+
+  def create
+  cart_item = CartItem.new(cart_item_params)
+  item = Item.find(params[:id])
+  cart_item.customer_id = current_customer.id
+  cart_item.item_id = item.id
+  redirect_to cart_items
+  end
+
+  def index
+    cart_item = CartItem.all
+  end
+
+  private
+
+  end
+
+
 end
