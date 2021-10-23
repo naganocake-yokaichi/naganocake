bc40c989e90bab0748caf9058c1eb53ae69bed36
sayakanoa <reiluv75418@gmail.com>
2021-10-23 01:46:10 +0000

    カートcreate機能

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -2,18 +2,19 @@ class Public::CartItemsController < ApplicationController
 
   def create
   cart_item = CartItem.new(cart_item_params)
-  item = Item.find(params[:id])
+  item = Item.find_by(params[:cart_item_id])
   cart_item.customer_id = current_customer.id
   cart_item.item_id = item.id
-  redirect_to cart_items
+  cart_item.save
+  redirect_to cart_items_path
   end
 
   def index
-    cart_items = CartItem.all
+    @cart_items = CartItem.all
   end
 
-  private
-
+  def cart_item_params
+    params.require(:cart_item).permit(:amount)
   end
 
 
