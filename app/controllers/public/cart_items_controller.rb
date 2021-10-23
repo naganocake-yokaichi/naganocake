2e85488b9b7cf630601fc53f067d449001447fb8
sayakanoa <reiluv75418@gmail.com>
2021-10-23 09:15:37 +0000

    create修正

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,37 +1,38 @@
 class Public::CartItemsController < ApplicationController
-  before_action :setup_cart_item!, only: %i[index create]
 
-  #cart_item = CartItem.new(cart_item_params)
-  #item = Item.find_by(params[:cart_item_id])
-  #cart_item.customer_id = current_customer.id
-  #cart_item.item_id = item.id
-  #cart_item.save
-  #redirect_to cart_items_path
+  #before_action :set_cart_items, only: [:create, :index, :destroy]
+  before_action :set_customer
 
   def create
-    @cart_item ||= current_cart.cart_items.build(item_id: params[:item_id])
-    @cart_item.quantity += params[:quantity].to_i
+    @cart_item = @customer.cart_items.new(cart_item_params)
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+    @cart = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+    @cart.amount += params[:cart_item][:amount].to_i
     @cart_item.save
     redirect_to cart_items_path
+    else
+    @cart_item.save
+    redirect_to cart_items_path
+    end
+  end
+
+  def index
+    @cart_items = current_customer.cart_items
   end
 
 
-  def index
-    @cart_items = current_cart.cart_items.includes([:item])
-    #@item_tax_price = (@item.price*1.1).round
+  private
+
+  def set_customer
+    @customer = current_customer
+  end
+
+  def set_cart_items
+    @cart_item = current_customer.cart_items.find_by(item_id: params[:item_id])
   end
 
   def cart_item_params
-    params.require(:cart_item).permit(:amount)
+    params.require(:cart_item).permit(:amount, :item_id)
   end
 
-  def destroy
-    cart_item = CartItem.find(params[:id])
-    cart_item.destroy
-    redirect_to cart_items_path
-  end
-
-  def setup_cart_item!
-    @cart_item = current_cart.cart_items.find_by(item_id: params[:item_id])
-  end
 end
