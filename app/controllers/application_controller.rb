df70d0196068e46f2d75f141dfc8c037ff34e269
sayakanoa <reiluv75418@gmail.com>
2021-10-23 03:54:11 +0000

    カート機能create変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,26 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
-  
-  
+
+
   protected
-  
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys:[:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number])
   end
-  
+
+   helper_method :current_cart
+
+  def current_cart
+    if current_customer
+      # ユーザーとカートの紐付け
+      current_cart = current_customer.cart_items || current_customer.create_cart_items!
+    else
+      # セッションとカートの紐付け
+      current_cart = CartItem.find_by(id: session[:customer_id]) || Cart.create
+      session[:customer_id] ||= current_cart.id
+    end
+    current_cart
+  end
+
 end
--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,21 +1,37 @@
 class Public::CartItemsController < ApplicationController
+  before_action :setup_cart_item!, only: %i[index create]
+
+  #cart_item = CartItem.new(cart_item_params)
+  #item = Item.find_by(params[:cart_item_id])
+  #cart_item.customer_id = current_customer.id
+  #cart_item.item_id = item.id
+  #cart_item.save
+  #redirect_to cart_items_path
 
   def create
-  cart_item = CartItem.new(cart_item_params)
-  item = Item.find_by(params[:cart_item_id])
-  cart_item.customer_id = current_customer.id
-  cart_item.item_id = item.id
-  cart_item.save
-  redirect_to cart_items_path
+    @cart_item ||= current_cart.cart_items.build(item_id: params[:item_id])
+    @cart_item.quantity += params[:quantity].to_i
+    @cart_item.save
+    redirect_to cart_items_path
   end
 
+
   def index
-    @cart_items = CartItem.all
+    @cart_items = current_cart.cart_items.includes([:item])
+    #@item_tax_price = (@item.price*1.1).round
   end
 
   def cart_item_params
     params.require(:cart_item).permit(:amount)
   end
 
+  def destroy
+    cart_item = CartItem.find(params[:id])
+    cart_item.destroy
+    redirect_to cart_items_path
+  end
 
+  def setup_cart_item!
+    @cart_item = current_cart.cart_items.find_by(item_id: params[:item_id])
+  end
 end
