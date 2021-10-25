b9126bdd7ddba75f34f4df689bff295773567fac
yamauchimiku <yamamochi7171@gmail.com>
2021-10-25 04:03:20 +0000

    add

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,2 +1,12 @@
 class Admin::OrderDetailsController < ApplicationController
+  def updated
+    @order_detail = OrderDetail.find(params[:id])
+    @order = @order_detail.order
+    @order_detail.update(order_params)
+  end
+
+  def order_params
+     params.require(:order_detail).permit(:make_status)
+  end
+
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,15 @@
 class Admin::OrdersController < ApplicationController
+  def show
+    @order = Order.find(params[:id])
+  end
+  def updated
+    @order = Order.find(params[:id])
+    @order_detail = @order.order_details
+    @order.update(order_params)
+  end
+
+  private
+  def order_params
+     params.require(:order).permit(:status)
+  end
 end
