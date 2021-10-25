2e1a4bb82091b7bda8163938c86421e91a3e6918
elmo0509 <elmo.boof@gmail.com>
2021-10-25 01:18:04 +0000

    enum日本語化設定(orderのstatus)

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details
-  
+
   # enumを定義する
   enum pay_method: { credit_card: 0, transfer: 1 }
+  enum status: { pay_waiting: 0, confirmation: 1, make: 2, readey: 3, sent: 4 }
 end
