b6ea26831dbe3a958a0ca82f434a33e13e49e533
elmo0509 <elmo.boof@gmail.com>
2021-10-25 01:14:10 +0000

    public/order_new,confirm,create,thanks,index画面作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -19,23 +19,39 @@ class Public::OrdersController < ApplicationController
       @order.name = Address.find(params[:order][:registered]).name
       @order.address = Address.find(params[:order][:registered]).address
       @order.postcode = Address.find(params[:order][:registered]).postcode
-
     end
 
-    
+    # sum = 0とし、初期値を設定する。
+    # アイテムの値段＊個数を品数分足していく。
+    # item.add_tax_priceはitem.rbに消費税分(1.1)をかけるという式を定義したもの。
+    sum = 0
+    cart_items = current_customer.cart_items
+    cart_items.each do |cart_item|
+      sum += (cart_item.item.add_tax_price * cart_item.amount)
+    end
+
+    # sumで商品の合計が出たので、ここに送料を足す。
+    # 以上で、請求金額を定義出来るようになる。
+    sum += @order.carriage
+    @order.total_payment = sum
+
   end
 
   def create
-    @customer = current_customer
-    @cart_items = @customer.cart_items.all
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    cart_items = current_customer.cart_items
     @order.save
     redirect_to orders_thanks_path
+    # データを保存した後に、カートの中身を全て空にする。
+    cart_items.destroy_all
   end
 
   def thanks
   end
 
   def index
+    @orders = Order.all
   end
 
   def show
@@ -44,7 +60,7 @@ class Public::OrdersController < ApplicationController
 
 private
   def order_params
-    params.require(:order).permit(:name, :address, :postcode, :total_payment)
+    params.require(:order).permit(:postcode, :address, :name, :carriage, :total_payment, :pay_method, :status)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,8 @@ class Item < ApplicationRecord
   belongs_to :genre
   has_many :cart_items
   has_many :order_details
+  
+  def add_tax_price
+    (self.price * 1.1).floor
+  end
 end
