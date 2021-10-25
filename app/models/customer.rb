0aedb280ca3b458a0114c054500a96588b568539
ymasa1213 <ymasa1213@yahoo.co.jp>
2021-10-25 12:00:15 +0000

    devise model日本語化　flash validate

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,6 +7,10 @@ class Customer < ApplicationRecord
   has_many :addresses, dependent: :destroy
   has_many :orders, dependent: :destroy
 
+  validates :last_name, :first_name, :email, :phone_number, presence: true
+  validates :last_name_kana, :first_name_kana,presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
+  validates :postcode, length: {is: 7}, numericality:{ only_integer: true }
+  validates :phone_number, numericality:{ only_integer: true }
   def active_for_authentication?
     super && (self.is_deleted == false)
   end
