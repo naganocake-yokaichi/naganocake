2ae5035e20c09f8c3a6188ee6ab491ad7d7c73a7
ymasa1213 <ymasa1213@yahoo.co.jp>
2021-10-26 03:17:21 +0000

    header修正

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
-    resources :orders, only: [:index, :show, :update] do
+    resources :orders, only: [:show, :update] do
       resources :order_details, only: [:update]
     end
   end
