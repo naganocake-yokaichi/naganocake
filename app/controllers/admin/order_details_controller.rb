a3681a2fbf35886862aa53a2aa093f27fafc057e
elmo0509 <elmo.boof@gmail.com>
2021-10-26 12:39:33 +0000

    orderのステータス自動変更機能

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -3,7 +3,22 @@ class Admin::OrderDetailsController < ApplicationController
     @order_detail = OrderDetail.find(params[:id])
     @order = @order_detail.order
     @order_detail.update(order_params)
-    redirect_to admin_order_path(@order)
+    if @order_detail.make_status_before_type_cast == 2
+      @order.update(status: 2)
+      redirect_to request.referer
+    elsif @order_detail.make_status_before_type_cast == 3
+      @order.order_details.each do |order_detail|
+        if order_detail.make_status_before_type_cast == 3
+          @order.update(status: 3)
+        else
+          @order.update(status: 2)
+          break
+        end
+      end
+      redirect_to request.referer
+    else
+      redirect_to request.referer
+    end
   end
 
   def order_params
