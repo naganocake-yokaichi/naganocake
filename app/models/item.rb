d78b0efb81861196f0da5edde59ab50d9811f609
elmo0509 <elmo.boof@gmail.com>
2021-10-27 02:20:28 +0000

    [Fix]is_activeのバリデーション設定変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,8 +9,8 @@ class Item < ApplicationRecord
   validates :introduction, presence: true
   validates :price, presence: true
   validates :genre_id, presence: true
-  validates :is_active, presence: true
-  
+  validates :is_active, inclusion: {in: [true, false]}
+
   def add_tax_price
     (self.price * 1.1).floor
   end
